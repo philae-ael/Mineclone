9aa2d1246aadbcfca4837f867ed0a449b72005f1
philae-ael <1511446+philae-ael@users.noreply.github>
2021-12-16 17:09:12 +0100

    proper use of nativeBuildInput to allow gcc/llvm switch

--- shell.nix
+++ shell.nix
@@ -1,28 +1,72 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell.override { stdenv = pkgs.stdenvNoCC; } {
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell.override
+{
+  stdenv = pkgs.llvmPackages_latest.stdenv;
+  # stdenv = pkgs.stdenvGcc11;
+}
+{
   buildInputs = with pkgs; [
-    conan cmake pkg-config
-    buildPackages.gcc11
-
     # system libs
-    # I dont use all that lol, but conan ant all of them
-    xorg.libX11 xorg.libfontenc libGL xorg.libICE xorg.libSM xorg.libXau xorg.libXaw xorg.libXcomposite xorg.libXcursor
-    xorg.libXdamage xorg.libXdmcp xorg.libXft xorg.libXtst xorg.libXinerama xorg.libxkbfile xorg.libXrandr
-    xorg.libXres xorg.libXScrnSaver xorg.libXvMC xorg.xtrans xorg.xcbutil.dev xorg.xcbutilcursor.dev xorg.xcbutilerrors 
-    xorg.xcbutilkeysyms.dev xorg.xcbutilrenderutil.dev xorg.xcbutilwm.dev xorg.libXxf86vm xorg.libXv xkeyboard_config 
-    xorg.libXpm libglvnd.dev mesa_drivers mesa_glu.dev mesa.dev xlibs.libxcb.dev xlibs.xcbutilimage.dev xlibs.xcbutilwm.dev
-    xlibs.xorgserver.dev xorg.libpthreadstubs xorg.libX11.dev xorg.libxcb.dev xorg.libXext.dev xorg.libXi.dev 
-    xorg.xcbproto xorg.xorgproto
+    # I dont use all that lol, but conan want all of them
+    xorg.libX11
+    xorg.libfontenc
+    libGL
+    xorg.libICE
+    xorg.libSM
+    xorg.libXau
+    xorg.libXaw
+    xorg.libXcomposite
+    xorg.libXcursor
+    xorg.libXdamage
+    xorg.libXdmcp
+    xorg.libXft
+    xorg.libXtst
+    xorg.libXinerama
+    xorg.libxkbfile
+    xorg.libXrandr
+    xorg.libXres
+    xorg.libXScrnSaver
+    xorg.libXvMC
+    xorg.xtrans
+    xorg.xcbutil.dev
+    xorg.xcbutilcursor.dev
+    xorg.xcbutilerrors
+    xorg.xcbutilkeysyms.dev
+    xorg.xcbutilrenderutil.dev
+    xorg.xcbutilwm.dev
+    xorg.libXxf86vm
+    xorg.libXv
+    xkeyboard_config
+    xorg.libXpm
+    libglvnd.dev
+    mesa_drivers
+    mesa_glu.dev
+    mesa.dev
+    xlibs.libxcb.dev
+    xlibs.xcbutilimage.dev
+    xlibs.xcbutilwm.dev
+    xlibs.xorgserver.dev
+    xorg.libpthreadstubs
+    xorg.libX11.dev
+    xorg.libxcb.dev
+    xorg.libXext.dev
+    xorg.libXi.dev
+    xorg.xcbproto
+    xorg.xorgproto
+  ];
 
-
-
-
+  nativeBuildInputs = with pkgs; [
+    # I put it here so that its path is well set
+    clang-tools
+    conan
+    cmake
+    pkg-config
 
     # Language servers
     cmake-language-server
-    clang-tools
-    ccls
-    unzip zip curl
+    unzip
+    zip
+    curl
 
   ];
 }
