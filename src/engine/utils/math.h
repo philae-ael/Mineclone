9659f724e51f3d05c558005ea68fdaacf6b4a742
philae-ael <1511446+philae-ael@users.noreply.github>
2021-12-18 11:30:24 +0100

    add matrices support into math header

--- src/engine/utils/math.h
+++ src/engine/utils/math.h
@@ -2,85 +2,152 @@
 #define MATH_H_
 
 #include <algorithm>
+#include <cstring>
 #include <functional>
 #include <numeric>
 namespace math {
 
-template <typename T, int N>
-struct vec {
+template <typename T, int N, int M>
+struct mat {
+   public:
+   private:
+    // This structs allow to make vec and mat only one class while having
+    // different kinds of accessors
+    template <int M_>
+    struct details {
+        using value_type = mat<T, M_, 1>;
+        using reference = value_type &;
+        using const_reference = const value_type &;
+        using pointer = value_type *;
+        using iterator = value_type *;
+        using const_iterator = const value_type *;
+
+        using storage_kind = value_type[N];  // NOLINT
+        constexpr static typename value_type::const_reference get(
+            const storage_kind &data, int i, int j) {
+            return data[i].at(j);
+        }
+        constexpr static typename value_type::reference get(storage_kind &data,
+                                                            int i, int j) {
+            return data[i].at(j);
+        }
+    };
+
+    template <>
+    struct details<1> {
+        using value_type = T;
+        using reference = value_type &;
+        using const_reference = const value_type &;
+        using pointer = value_type *;
+        using iterator = value_type *;
+        using const_iterator = const value_type *;
+
+        using storage_kind = value_type[N];  // NOLINT
+        constexpr static const_reference get(const storage_kind &data, int i,
+                                             int /*j*/) {
+            return data[i];
+        }
+        constexpr static reference get(storage_kind &data, int i, int /*j*/) {
+            return data[i];
+        }
+    };
+
+   public:
     using value_type = T;
     using reference = value_type &;
     using const_reference = const value_type &;
     using pointer = value_type *;
-    using iterator = value_type *;
-    using const_iterator = const value_type *;
-    value_type data[N];  // NOLINT: better than wrapping std::array
 
-    constexpr reference operator[](const int i) { return data[i]; }
-    constexpr const_reference operator[](int i) const { return data[i]; }
+    using value_stored = typename details<M>::value_type;
+    using reference_stored = typename details<M>::reference;
+    using const_reference_stored = typename details<M>::const_reference;
+    using pointer_stored = typename details<M>::pointer;
+
+    using iterator = typename details<M>::iterator;
+    using const_iterator = typename details<M>::const_iterator;
+
+    typename details<M>::storage_kind
+        data;  // NOLINT: this is intended to be public: this struct meant to be
+               // a POD
+
+    constexpr reference_stored operator[](int i) { return at(i); }
+    constexpr const_reference_stored operator[](int i) const { return at(i); }
+
+    constexpr reference_stored at(int i) { return data[i]; }
+    constexpr const_reference_stored at(int i) const { return data[i]; }
+
+    constexpr reference at(int i, int j) { return details<M>::get(data, i, j); }
+    constexpr const_reference at(int i, int j) const {
+        return details<M>::get(data, i, j);
+    }
+
+    constexpr mat<T, M, 1> row(int i) const {
+        mat<T, M, 1> res;
+        std::copy(data[i], data[i + 1], res.data);
+    }
+
     constexpr iterator begin() { return iterator(data); }
     constexpr iterator end() { return iterator(data + N); }
-    constexpr const_iterator cbegin() const { return iterator(data); }
-    constexpr const_iterator cend() const { return iterator(data + N); }
+    constexpr const_iterator cbegin() const { return const_iterator(data); }
+    constexpr const_iterator cend() const { return const_iterator(data + N); }
 
-    constexpr vec &operator-() {
+    constexpr mat &operator-() {
         std::transform(this->cbegin(), this->cend(), this->begin(),
                        [](auto x) { return -x; });
         return *this;
     }
 
-    constexpr friend bool operator==(const vec<T, N> &lhs,
-                                     const vec<T, N> &rhs) {
+    constexpr friend bool operator==(const mat &lhs, const mat &rhs) {
         return std::equal(lhs.cbegin(), lhs.cend(), rhs.cbegin());
     }
 
-    constexpr vec &operator+=(const vec &rhs) {
+    constexpr mat &operator+=(const mat &rhs) {
         std::transform(this->cbegin(), this->cend(), rhs.cbegin(),
                        this->begin(), std::plus{});
         return *this;
     }
 
-    constexpr vec &operator-=(const vec &rhs) {
+    constexpr mat &operator-=(const mat &rhs) {
         std::transform(this->cbegin(), this->cend(), rhs.cbegin(),
                        this->begin(), std::minus{});
         return *this;
     }
 
-    constexpr vec &operator*=(const vec &rhs) {
+    constexpr mat &operator*=(const mat &rhs) {
         std::transform(this->cbegin(), this->cend(), rhs.cbegin(),
                        this->begin(), std::multiplies{});
         return *this;
     }
 
-    constexpr vec &operator/=(const vec &rhs) {
+    constexpr mat &operator/=(const mat &rhs) {
         std::transform(this->cbegin(), this->cend(), rhs.cbegin(),
                        this->begin(), std::divides{});
         return *this;
     }
 
-    constexpr vec friend operator*(const vec<T, N> &lhs, const vec<T, N> &rhs) {
-        vec res;
+    constexpr mat friend operator*(const mat &lhs, const mat &rhs) {
+        mat res;
         std::transform(lhs.cbegin(), lhs.cend(), rhs.cbegin(), res.begin(),
                        std::multiplies{});
         return res;
     }
 
-    constexpr vec friend operator/(const vec<T, N> &lhs, const vec<T, N> &rhs) {
-        vec res;
+    constexpr mat friend operator/(const mat &lhs, const mat &rhs) {
+        mat res;
         std::transform(lhs.cbegin(), lhs.cend(), rhs.cbegin(), res.begin(),
                        std::divides{});
         return res;
     }
 
-    constexpr vec friend operator+(const vec<T, N> &lhs, const vec<T, N> &rhs) {
-        vec res;
+    constexpr mat friend operator+(const mat &lhs, const mat &rhs) {
+        mat res;
         std::transform(lhs.cbegin(), lhs.cend(), rhs.cbegin(), res.begin(),
                        std::plus{});
         return res;
     }
 
-    constexpr vec friend operator-(const vec<T, N> &lhs, const vec<T, N> &rhs) {
-        vec res;
+    constexpr mat friend operator-(const mat &lhs, const mat &rhs) {
+        mat res;
         std::transform(lhs.cbegin(), lhs.cend(), rhs.cbegin(), res.begin(),
                        std::minus{});
         return res;
@@ -91,51 +158,94 @@ struct vec {
     // Note: There is no operator/, operator- (because i want the usual order
     // scalar `op` to be respected), use resp. (1/x)*v, (-x)*v
     //
-    constexpr vec friend operator*(const value_type scalar,
-                                   const vec<T, N> &rhs) {
-        vec res;
+    constexpr mat friend operator*(const value_type scalar, const mat &rhs) {
+        mat res;
         std::transform(
             rhs.cbegin(), rhs.cend(), res.begin(),
             [scalar](const value_type &rhs_) { return scalar * rhs_; });
         return res;
     }
-    constexpr vec friend operator+(const value_type scalar,
-                                   const vec<T, N> &rhs) {
-        vec res;
+    constexpr mat friend operator+(const value_type scalar, const mat &rhs) {
+        mat res;
         std::transform(
             rhs.cbegin(), rhs.cend(), res.begin(),
             [scalar](const value_type &rhs_) { return scalar + rhs_; });
         return res;
     }
-
-    constexpr value_type norm2() const { return dot(*this, *this); }
-
-    constexpr bool near(const vec &other, value_type eps) const {
-        return (*this - other)->norm2() < eps;
-    }
 };
+
+
+template <typename T, int N, int M>
+constexpr mat<T, M, N> transpose(const mat<T, N, M> &m){
+    mat<T, M, N> res;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            res.at(j, i) = m.at(i, j);
+        }
+    }
+    return res;
+}
+
+// Quite a naive implementation, hopefully, should be optimized out by compiler
+// for small values of L, M, N
+template <typename T, int L, int M, int N>
+constexpr mat<T, L, N> dot(const mat<T, L, M> &lhs, const mat<T, M, N> &rhs) {
+    mat<T, L, N> res;
+    mat<T, N, M> rhsT = transpose(rhs); // should allow better cache locality
+    for (int i = 0; i < L; i++) {
+        for (int j = 0; j < N; j++) {
+            res.at(i, j) = dot(mat{lhs.at(i)}, mat{rhsT.at(j)});
+        }
+    }
+    return res;
+}
+
+
+// vector specific functions
+
+template <typename T, int N>
+using vec = mat<T, N, 1>;
 
 template <typename T, int N>
 constexpr typename vec<T, N>::value_type dot(const vec<T, N> &lhs,
                                              const vec<T, N> &rhs) {
-    const vec tmp = lhs * rhs;
+    const mat tmp = lhs * rhs;
     return std::accumulate(tmp.cbegin(), tmp.cend(),
                            typename vec<T, N>::value_type{});
 }
 
+template <typename T, int N>
+constexpr typename vec<T, N>::value_type norm2(const vec<T, N> &v) {
+    return dot(v, v);
+}
+
 template <typename T>
 constexpr vec<T, 3> wedge(const vec<T, 3> &lhs, const vec<T, 3> &rhs) {
-    return {lhs[1] * rhs[2] - lhs[2] * rhs[1],
-            lhs[2] * rhs[0] - lhs[0] * rhs[2],
-            lhs[0] * rhs[1] - lhs[1] * rhs[0]};
+    return {lhs.data[1] * rhs.data[2] - lhs.data[2] * rhs.data[1],
+            lhs.data[2] * rhs.data[0] - lhs.data[0] * rhs.data[2],
+            lhs.data[0] * rhs.data[1] - lhs.data[1] * rhs.data[0]};
 };
 
+using vec2f = vec<float, 2>;
+using mat2f = mat<float, 2, 2>;
 using vec3f = vec<float, 3>;
+using mat3f = mat<float, 3, 3>;
 using vec4f = vec<float, 4>;
+using mat4f = mat<float, 4, 4>;
+
+using vec2d = vec<double, 2>;
+using mat2d = mat<double, 2, 2>;
 using vec3d = vec<double, 3>;
+using mat3d = mat<double, 3, 3>;
 using vec4d = vec<double, 4>;
+using mat4d = mat<double, 4, 4>;
+
+using vec2i = vec<int, 2>;
+using mat2i = mat<int, 2, 2>;
 using vec3i = vec<int, 3>;
+using mat3i = mat<int, 3, 3>;
 using vec4i = vec<int, 4>;
+using mat4i = mat<int, 4, 4>;
 
 }  // namespace math
 #endif  // !MATH_H_
--- test/math.cpp
+++ test/math.cpp
@@ -3,21 +3,32 @@
 #include <catch2/catch.hpp>
 #include <iostream>
 
+using math::mat2i;
 using math::vec3i;
 
-TEST_CASE("vec basic operations") {
-    std::cout << sizeof(vec3i) << "\n";
-
+TEST_CASE("math::vec basic operations") {
     STATIC_REQUIRE(vec3i{0, 0, 0} == vec3i{});
     STATIC_REQUIRE(vec3i{1, 2, 3} + vec3i{3, 2, 1} == vec3i{4, 4, 4});
     STATIC_REQUIRE(vec3i{1, 2, 3} - vec3i{3, 2, 1} == vec3i{-2, 0, 2});
     STATIC_REQUIRE(vec3i{1, 2, 3} * vec3i{3, 2, 1} == vec3i{3, 4, 3});
     STATIC_REQUIRE(vec3i{4, 6, 2} / vec3i{2, 2, 1} == vec3i{2, 3, 2});
     STATIC_REQUIRE(-vec3i{1, 2, 1} == vec3i{-1, -2, -1});
-
 }
 
-TEST_CASE("vec norm, wedge, norm2") {
+TEST_CASE("math::mat basic operations") {
+    STATIC_REQUIRE(mat2i{{{0, 0}, {0, 0}}} == mat2i{});
+    STATIC_REQUIRE(mat2i{{{0, 0}, {-1, -2}}} + mat2i{{{1, 2}, {2, 1}}} ==
+                   mat2i{{{1, 2}, {1, -1}}});
+    STATIC_REQUIRE(mat2i{{{0, 0}, {-1, -2}}} - mat2i{{{1, 2}, {2, 1}}} ==
+                   mat2i{{{-1, -2}, {-3, -3}}});
+    STATIC_REQUIRE(mat2i{{{0, 0}, {-1, -2}}} * mat2i{{{1, 2}, {2, 1}}} ==
+                   mat2i{{{0, 0}, {-2, -2}}});
+    STATIC_REQUIRE(mat2i{{{0, 0}, {1, 2}}} / mat2i{{{1, 2}, {1, 1}}} ==
+                   mat2i{{{0, 0}, {1, 2}}});
+    STATIC_REQUIRE(-mat2i{{{0, 0}, {-1, -2}}} == mat2i{{{0, 0}, {1, 2}}});
+}
+
+TEST_CASE("math::vec norm, wedge, norm2") {
     STATIC_REQUIRE(math::wedge(vec3i{1, 0, 0}, vec3i{0, 1, 0}) ==
                    vec3i{0, 0, 1});
     STATIC_REQUIRE(math::wedge(vec3i{0, 1, 0}, vec3i{1, 0, 0}) ==
@@ -26,11 +37,10 @@ TEST_CASE("vec norm, wedge, norm2") {
                    vec3i{1, 0, 0});
 
     STATIC_REQUIRE(math::dot(vec3i{1, 0, 0}, vec3i{0, 1, 0}) == 0);
-    STATIC_REQUIRE(vec3i{1, 2, 1}.norm2() == 6);
-
+    STATIC_REQUIRE(math::norm2(vec3i{1, 2, 1}) == 6);
 }
 
-TEST_CASE("vec +=/-=/...") {
+TEST_CASE("math::vec +=/-=/...") {
     auto l1 = []() constexpr {
         vec3i v{0, 0, 0};
         v += vec3i{1, 2, 3};
@@ -58,7 +68,36 @@ TEST_CASE("vec +=/-=/...") {
     STATIC_REQUIRE(l4() == vec3i{1, 3, 4});
 }
 
-TEST_CASE("vec scalar `op` vector") {
-    STATIC_REQUIRE(3*vec3i{1, 2, 3} == vec3i{3, 6, 9});
-    STATIC_REQUIRE(3+vec3i{1, 2, 3} == vec3i{4, 5, 6});
+TEST_CASE("math::vec scalar `op` vector") {
+    STATIC_REQUIRE(3 * vec3i{1, 2, 3} == vec3i{3, 6, 9});
+    STATIC_REQUIRE(3 + vec3i{1, 2, 3} == vec3i{4, 5, 6});
+}
+
+TEST_CASE("math::mat data access") {
+    STATIC_REQUIRE(vec3i{1, 2, 3}.at(1) == 2);
+    STATIC_REQUIRE(vec3i{1, 2, 3}.at(1, 0) == 2);
+    STATIC_REQUIRE(mat2i{{{2, 3}, {2, 3}}}.at(1, 0) == 2);
+}
+
+TEST_CASE("math::mat transpose") {
+    STATIC_REQUIRE(math::transpose(math::vec2i{1, 2}) ==
+                   math::mat<int, 1, 2>{{{1, 2}}});
+}
+
+TEST_CASE("math::mat dot") {
+    constexpr math::mat<int, 1, 2> m1{{{1, 2}}};
+    constexpr math::mat<int, 2, 1> m2{1, 2};
+
+    STATIC_REQUIRE(math::dot(m1, m2) == math::mat<int, 1, 1>{5});
+
+    constexpr math::mat<int, 2, 2> m3{{{0, 1}, {1, 0}}};
+    constexpr math::mat<int, 2, 3> m4{{{1, 2, 0}, {1, 2, 3}}};
+
+    STATIC_REQUIRE(math::dot(m1, m2) == math::mat<int, 1, 1>{5});
+    STATIC_REQUIRE(math::dot(m3, m4) ==
+                   math::mat<int, 2, 3>{{{1, 2, 3}, {1, 2, 0}}});
+
+    constexpr math::mat<int, 2, 2> m5{{{1, 0}, {0, -1}}};
+    constexpr math::vec2i v{1, 2};
+    STATIC_REQUIRE(math::dot(m5, v) == math::vec2i{1, -2});
 }
