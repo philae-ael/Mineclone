74a164d83ea4fcc37371ce1ccf1b24dfef856b2f
philae-ael <1511446+philae-ael@users.noreply.github>
2022-01-05 17:56:31 +0100

    add an utility iterator for sequence based loops

---
+++ src/engine/utils/range_iterator.h
@@ -0,0 +1,82 @@
+#ifndef RANGE_ITERATOR_H_
+#define RANGE_ITERATOR_H_
+
+#include <array>
+#include <concepts>
+#include <type_traits>
+
+#include "logging.h"
+
+// https://stackoverflow.com/a/36934374
+template<bool...> struct bool_pack;
+template<bool... bs> 
+using all_true = std::is_same<bool_pack<bs..., true>, bool_pack<true, bs...>>;
+
+template<bool... bs> 
+inline constexpr bool all_true_v = all_true<bs...>::value;
+
+
+template <std::unsigned_integral T, T... Bounds>
+class range_it {
+   public:
+    static constexpr std::size_t len = sizeof...(Bounds);
+    static_assert(len >= 1);
+    static_assert(all_true_v<(Bounds > 0)...>);
+
+    using seq_container = std::array<T, len>;
+    static constexpr seq_container bounds{Bounds...};
+
+    range_it& begin() {
+        auto l = log << "Range bounds: ";
+        for (auto b : bounds) l <<  b << " ";
+        return *this;
+    }
+
+    static range_it end() {
+        range_it r;
+        r.current_seq = bounds;
+        return r;
+    }
+
+    range_it operator++(int) {  // postfix++
+        range_it ret = *this;
+        ++(*this);
+        return ret;
+    }
+
+    range_it& operator++() {  // ++prefix
+        std::size_t i = 0;
+
+        // completed
+        [[unlikely]] if (current_seq[0] == bounds[0]) return *this;
+
+        for (; i < len; i++) {
+            current_seq[i] += 1;
+
+            [[likely]] if (current_seq[i] < bounds[i]) break;
+
+            current_seq[i] = 0;
+        }
+
+        // That means the range has done a full turn
+        if (i == len && current_seq[i - 1] == 0) current_seq = bounds;
+
+        return *this;
+    }
+
+    seq_container operator*() { return current_seq; }
+
+    bool operator==(const range_it& other) const {
+        if (current_seq[0] == bounds[0]) return true;  // completed
+
+        return current_seq == other.current_seq;
+    }
+
+    bool operator!=(const range_it& other) const { return !(*this == other); }
+
+   private:
+    Logger log {Logger::get("Range")};
+    seq_container current_seq{0};
+};
+
+#endif  // !RANGE_ITERATOR_H_
---
+++ test/range_it.cpp
@@ -0,0 +1,62 @@
+#include <array>
+#include <catch2/catch.hpp>
+
+#include "engine/utils/range_iterator.h"
+
+TEST_CASE("range_it 1d") {
+    range_it<unsigned int, 5> r;
+    REQUIRE((*r)[0] == 0);
+    r++;
+    REQUIRE((*r)[0] == 1);
+    r++;
+    REQUIRE((*r)[0] == 2);
+    r++;
+    REQUIRE((*r)[0] == 3);
+    r++;
+    REQUIRE((*r)[0] == 4);
+    r++;
+    REQUIRE(r == r.end());
+}
+
+TEST_CASE("range_it 1d for") {
+    range_it<unsigned int, 5> r;
+    std::array<unsigned int, 5> a{};
+    for (auto [i] : r) {
+        a[i] = i;
+    }
+    REQUIRE(a == std::array<unsigned int, 5>{0, 1, 2, 3, 4});
+}
+
+TEST_CASE("range_it 2d for") {
+    range_it<unsigned int, 3, 2> r;
+    std::array<std::array<unsigned int, 2>, 6> a{};
+    for (auto [i, j] : r) {
+        a[i + j*3] = std::array<unsigned int, 2>{i, j};
+    }
+    REQUIRE(a == std::array<std::array<unsigned int, 2>, 6> {{
+                     {0, 0},
+                     {1, 0},
+                     {2, 0},
+                     {0, 1},
+                     {1, 1},
+                     {2, 1},
+                 }});
+}
+
+TEST_CASE("range_it 3d for") {
+    range_it<unsigned int, 2, 2, 2> r;
+    std::array<std::array<unsigned int, 3>, 8> a{};
+    for (auto [i, j, k] : r) {
+        a[i + j*2 + k*4] = std::array<unsigned int, 3>{i, j, k};
+    }
+    REQUIRE(a == std::array<std::array<unsigned int, 3>, 8> {{
+                     {0, 0, 0},
+                     {1, 0, 0},
+                     {0, 1, 0},
+                     {1, 1, 0},
+                     {0, 0, 1},
+                     {1, 0, 1},
+                     {0, 1, 1},
+                     {1, 1, 1},
+                 }});
+}
